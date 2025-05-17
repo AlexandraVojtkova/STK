b778f5d4e1f8e426b1607f43a0e22fec15f19037
Alexandra Vojtková <avojtkova@student.umb.sk>
2025-05-17 16:13:05 +0200

    ignorovanie hesla

--- src/test/java/sk/umb/systemSTK/utils/UserDTO.java
+++ src/test/java/sk/umb/systemSTK/utils/UserDTO.java
@@ -4,6 +4,7 @@ package sk.umb.systemSTK.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
 
 @Data
 @AllArgsConstructor
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
     Long userId;
     String userName;
+    @JsonIgnore
     String password;
 }
