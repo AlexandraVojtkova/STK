17d5e8a8df5d634a4f9b7ab7643448e2433e09a4
Alexandra Vojtková <avojtkova@student.umb.sk>
2025-05-17 16:13:50 +0200

    tvorba controllerov

--- src/test/java/sk/umb/systemSTK/controllers/TechnicianController.java
+++ src/test/java/sk/umb/systemSTK/controllers/TechnicianController.java
@@ -1,4 +1,40 @@
 package sk.umb.systemSTK.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import sk.umb.systemSTK.services.ExcelProcessor;
+import sk.umb.systemSTK.services.TechnicianService;
+import sk.umb.systemSTK.utils.TechnicianDTO;
+
+@RestController
 public class TechnicianController {
+    @Autowired
+    TechnicianService technicianService;
+
+    @Autowired
+    ExcelProcessor processor;
+
+    @GetMapping("/api/getTechnicianById/{id}")
+    public TechnicianDTO getTechnicianById(@PathVariable Long id) {return technicianService.getTechnicianById(id);}
+
+    @PostMapping("/api/createTechnician")
+    public void createTechnician(@RequestBody TechnicianDTO technicianDTO) {technicianService.putTechnician(technicianDTO);}
+
+    @PostMapping("api/uploadData/ek")
+    public ResponseEntity<?> uploadExcelFileForEk(@RequestParam("file") MultipartFile file) {
+        processor.processUploadedExcelFileForEk(file);
+        return ResponseEntity.ok("Upload successful");
+    }
+    @PostMapping("api/uploadData/tk")
+    public ResponseEntity<?> processUploadedPdfForTk(@RequestParam("file") MultipartFile file) {
+        processor.processUploadedPdfForTk(file);
+        return ResponseEntity.ok("Upload successful");
+    }
+    @PostMapping("api/uploadData/ko")
+    public ResponseEntity<?> uploadExcelFileForKo(@RequestParam("file") MultipartFile file) {
+        processor.processUploadedExcelFileForKo(file);
+        return ResponseEntity.ok("Upload successful");
+    }
 }
